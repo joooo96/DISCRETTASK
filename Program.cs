daf564d8b5f8e2b6867b60f6029fd1b9d6726e54
joooo96 <121642102+joooo96@users.noreply.github.com>
2022-12-29 23:12:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace project_1_prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, i, j, f;
+
+            Console.Write("Enter first number : ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Prime numbers between " + "{0} and {1} are: ", x, y);
+
+            for (i = x; i <= y; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+                f = 1;
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        f = 0;
+                        break;
+                    }
+                }
+                if (f == 1)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
